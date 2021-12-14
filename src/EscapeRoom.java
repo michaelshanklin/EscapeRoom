ac32b397535f09734d65566503d75bd7f31f052d
Michael Shanklin <shankmic002@warren.k12.in.us>
2021-12-14 12:09:14 -0500

    Escape Room Project

--- src/EscapeRoom.java
+++ src/EscapeRoom.java
@@ -1,7 +1,37 @@
+import javax.swing.*;
+
 public class EscapeRoom {
 
-   public static void main(String [] args){
+    /*Make sure it is difficult to exit the dangerous room*/
+
+    public static void main(String[] args) {
 
 
-   }
-}
+        int sizeOfHead = Integer.parseInt(JOptionPane.showInputDialog("So how big is your head?"));
+        int numberOfDates = Integer.parseInt(JOptionPane.showInputDialog("How many girls dated you?"));
+        int timesCakeWasConsumed = Integer.parseInt(JOptionPane.showInputDialog("How much cake did you eat?"));
+
+        if (sizeOfHead >= 30) ;
+        System.out.println("Your head will fit through the door. Have a nice day");
+
+        if (numberOfDates >= 5) {
+            System.out.println("Girls like you. You may leave.");
+        } else {
+
+            if (timesCakeWasConsumed >= 2) {
+                System.out.println("You have saved cake for others. Leave.");
+
+            } else {
+                System.out.println("You may not escape.");
+            }else{
+                System.out.println("Not enough girls like you. You can't escape");
+            }else{
+                System.out.println("You have eaten all the cake. You may not leave.");
+            }
+
+        }
+    
+
+
+
+
